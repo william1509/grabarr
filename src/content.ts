d294b9ea8098960ef48f333c36feacfd61eece4b
william <william_martineau@hotmail.com>
2023-10-21 12:16:24 -0400

    wip

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,11 @@
 console.log("ffdsfsd")
 
+let mousePosition = {x: 0, y: 0};
+document.addEventListener('mousemove', (e: MouseEvent) => {
+  mousePosition.x = e.pageX;
+  mousePosition.y = e.pageY;
+});
+
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     console.log("Received message from background script!", message)
     switch (message.type) {
@@ -8,7 +14,14 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
         var div=document.createElement("div"); 
         document.body.appendChild(div); 
         div.innerText="test123";
+        div.style.position = "absolute";
+        div.style.left = mousePosition.x + "px";
+        div.style.top = mousePosition.y - 100 + "px";
+        div.style.maxWidth = "fit-content";
         sendResponse("here");
+        break;
+      default:
+        console.log("unknown message type", message.type);
         break;
     }
   });
